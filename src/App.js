97c719029da97857aa77777bc3e0c2924bc3c589
Bernabe <bernabe@maniak.com.mx>
2018-01-28 19:16:26 -0800

    Add prettier conf file. Proof of concept

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="item item-one">1</div>
+        <div className="item item-two">2</div>
+        <div className="item item-three">3</div>
+        <div className="item item-four">4</div>
+        <div className="item item-five">5</div>
+        <div className="item item-six">6</div>
+        <div className="item item-seven">7</div>
+        <div className="item item-eight">8</div>
+        <div className="item item-nine">9</div>
       </div>
     );
   }
